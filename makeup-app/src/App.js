f76edbdba7f6f321ec7f92a19dbc4ce6afe880b9
Neematrasheed05 <nymatrasheed05@gmail.com>
2023-11-07 12:36:29 +0300

    complete

--- makeup-app/src/App.js
+++ makeup-app/src/App.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [products , setProducts] = useState([]);
+  useEffect(() => {
+    // Fetch data when the component mounts
+    fetch('http://makeup-api.herokuapp.com/api/v1/products.json')
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data)
+        setProducts(data);
+        
+      });
+  }, []);
   return(
     <div>
-      my main 
+      {products.map((product) => (
+        <h2 key={product.id}>{product.name}</h2>
+        ))}
     </div>
   );
 }
