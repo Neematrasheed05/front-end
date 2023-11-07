59b46955adcadd9c69e224021d37bd8d5f92fc7f
Neematrasheed05 <nymatrasheed05@gmail.com>
2023-11-07 15:20:29 +0300

    displayed home page

--- makeup-app/src/ProductCollection.jsx
+++ makeup-app/src/ProductCollection.jsx
@@ -5,12 +5,26 @@ function ProductCollection() {
   const products = useContext(UserContext);
 
   return (
-    <div>
-      {products.map((product) => (
-        <div key={product.id}>
-          <h1>{product.name}</h1>
-        </div>
-      ))}
+    <div className="container">
+      <div className="row">
+        {products.map((product) => (
+          <div key={product.id} className="col-lg-3 col-md-4 col-sm-6 mb-4">
+            <div className="card">
+              <img src={product.image_link} className="card-img-top" alt={product.name} />
+              <div className="card-body">
+                <h5 className="card-title">{product.brand}</h5>
+                <p className="card-text">{product.category}</p>
+                <p className="card-text">
+                  {product.price}
+                  
+                </p>
+               
+                <p className="card-text">{product.description}</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
