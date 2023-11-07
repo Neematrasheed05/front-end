c69d716063764d26a8f48c3d7478a467cb69e2cf
Neematrasheed05 <nymatrasheed05@gmail.com>
2023-11-07 15:39:44 +0300

    completed display page

--- makeup-app/src/ProductCollection.jsx
+++ makeup-app/src/ProductCollection.jsx
@@ -7,18 +7,19 @@ function ProductCollection() {
   return (
     <div className="container">
       <div className="row">
+      
         {products.map((product) => (
           <div key={product.id} className="col-lg-3 col-md-4 col-sm-6 mb-4">
-            <div className="card">
-              <img src={product.image_link} className="card-img-top" alt={product.name} />
+            <div className="card card-hover"> {/* Add card-hover class */}
+              <img src={product.image_link} className="card-img-top shadow-lg p-3 mb-5 bg-body-tertiary rounded" alt={product.name} />
               <div className="card-body">
                 <h5 className="card-title">{product.brand}</h5>
                 <p className="card-text">{product.category}</p>
                 <p className="card-text">
                   {product.price}
-                  
+                  <span>{product.price_sign}</span>
                 </p>
-               
+                
                 <p className="card-text">{product.description}</p>
               </div>
             </div>
